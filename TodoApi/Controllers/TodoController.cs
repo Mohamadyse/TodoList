4ee952695c4414d427312322acc86e5b4b84a2c7
Mohamad Eddin <mohamad.eddin@skandia.se>
2019-02-14 17:41:44 +0100

    get request added

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -22,10 +25,34 @@ namespace TodoApi.Controllers
             }
         }
 
+        // GET : api/ Todo
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var item = await _context.TodoItems.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
 
 
-    //    // GET: api/<controller>
-    //    [HttpGet]
+
+
+
+
+
+        //// GET: api/<controller>
+       // [HttpGet]
     //    public IEnumerable<string> Get()
     //    {
     //        return new string[] { "value1", "value2" };
